b0ca7c54ae5a9706a056dd7c64b2403ad62941b0
Scott Mcdermott <scott@smemsh.net>
2025-08-09 17:28:05 -0700

    + ghreltags: add caching of github tags api calls using a python shelf
    this is saving on api calls because otherwise it will be redoing all the
    repo tags api calls for every host in the play.  for public access the
    api limits are quite low; for private we can probably get away with
    looking only once per play.  what's nice is that now it doesn't matter
    how many hosts there are.
    
    right now this is only caching for the duration of the play but we might
    add a refresh= boolean to let it use the last-cached values.
    
    one caveat however: in order for it to work we need to use throttle=1
    (although it's implemented in a way that it won't be dangerous if this
    is elided, it just will cause tasks to fail if they exceed a timeout
    while waiting for a lock), but in practice that shouldn't be too much of
    a problem because it's only the set_fact itself (which obtains the api
    results) that needs to serialize execution, and all the subsequent ones
    after the first will be fully local because they'll hit the cache (the
    set_fact is always delegate_to localhost, doesn't make sense to execute
    the api calls on remote nodes).  so it should complete quite quickly
    even with large number of hosts, when throttle=1.
    
    in theory we could probably up this limit to batch it, as long as a
    single batch can always complete by the total sleep time it should work.
    so there's room for growth later... we could also implement cache aging
    for example, because the cache key is just a time_t value so we can just
    add seconds to this to get how long ago the original play was run that
    added the entry to the cache.
    
    overall this is quite an enhancement, allows it to scale into the
    future, and was fun to implement! very nice...

--- filters/ghreltags.py
+++ filters/ghreltags.py
@@ -8,6 +8,8 @@ desc:
   - can also run from cmdline with dict provided on stdin as json
   - tags looked up, filtered by prefix (in dict value) or provided default
   - dict returned with values replaced by highest tag via version sort
+  - uses cache key arg so tags api call happens only once per repo per play
+  - cache uses a lockfile for safety, but should be run only with throttle=1
 
 args:
   1: dictionary
@@ -17,7 +19,8 @@ args:
              - if empty, get defaulted from filter arg2
   2: github username reponames are relative to (required) [filter arg1]
   3: default prefix for empty keys (required)             [filter arg2]
-  4: api key / access token (optional) for github lookup  [filter arg3]
+  4: cache key str, eg ansible_date_time.epoch (required) [filter arg3]
+  5: api key / access token (optional) for github lookup  [filter arg4]
 
 """
 __url__     = 'https://github.com/smemsh/devskel/'
@@ -28,17 +31,24 @@ __devskel__ = '0.8.1'
 from sys import exit, hexversion
 if hexversion < 0x030900f0: exit("minpython: %s" % hexversion)
 
-import argparse, json, re
+import argparse, shelve, json, re
 import urllib.request as rq
 
 from sys import argv
 from sys import stdin, stdout, stderr
+from time import sleep
+from stat import S_IRUSR, S_IWUSR, S_IRGRP, S_IWGRP, S_IROTH
+from fcntl import flock, LOCK_EX, LOCK_UN, LOCK_NB
+from errno import EAGAIN, EWOULDBLOCK, EACCES
 from select import select
 
-from os.path import basename
+from os.path import basename, dirname
 from os import (
+    makedirs,
     getenv, unsetenv,
     isatty, dup,
+    O_RDWR, O_CREAT,
+    open as osopen,
     close as osclose,
     EX_OK as EXIT_SUCCESS,
     EX_SOFTWARE as EXIT_FAILURE,
@@ -66,12 +76,113 @@ headers = {
     'X-GitHub-Api-Version': GHAPIVER,
 }
 
+CACHEFILE = f"{getenv('HOME')}/.cache/ghreltags.db"
+CACHELOCK = f"{CACHEFILE}.lock"
+LOCKSLEEP = 3
+LOCKTRIES = 3
+
+#
+
+# we cannot use the FilterModule constructor to provision the cache and
+# lock, because ansible creates a FilterModule instance for every
+# defined filter in every fork (apparently, tested with a dummy),
+# regardless of whether they get used by the play.  so we create our own
+# class for the cache, and only instantiate if the filter gets called.
+#
+# note: in theory there's only one FilterModule instance per fork so we
+# might have just used class variables to store the tags list and it
+# would work with throttle=1, however: (1) implementing it with an
+# external cache protects against accidental use with parallel forks;
+# (2) we can also protect from rearchitecture or changed semantics of
+# FilterModule by later ansible development; (3) we can implement an
+# age-based cache later by persisting it; (4) we could pass a special
+# cachekey value (like 0) to use the last stored cache contents; (5) we
+# could eventually populate the shelf db through some external means
+# that avoids the API altogether (as we control all the repositories
+# ourselves).
+#
+class RelTagsCache(object):
+
+    def __init__(self):
+
+        # all the FilterModules are independent so we cannot share data
+        # amongst instances.  we create a persistent cache and try to
+        # open it for each run so the filter functions can access it.
+        # the cache's purpose is to reduce API requests for tags list
+        # lookups.  we delegate_to the controller but we still populate
+        # the facts for each host separately (ie delegate_facts: false)
+        # because each host can have different repos (although usually
+        # they don't).  we store repository tag list api fetch results
+        # in the cache, so really there is only one api request per repo
+        # (but then also per result page) no matter how many hosts we're
+        # running the play on.  (github is quite stingy with api
+        # requests and even this many will be problematic unless we're
+        # authenticated, which is why we take apitok parameter.)
+        #
+        # to implement the cache we use a python shelve object, which is
+        # actually just a sqlite3 db that stores a pickled blob for the
+        # value of each dictionary key that we store.  in the cache
+        # object constructor, we open a protective global lock file and
+        # refuse to proceed if we cannot get a lock (after some
+        # sleep/retry attempts).  the lock is for safety, and we bail if
+        # we can't acquire it, but this is only a guard.  the task that
+        # uses this filter should (1) use it only once in the particular
+        # task's template, (2) use strategy=linear if any other task
+        # uses the filter, and (3) use throttle=1 on the task, so other
+        # hosts will run serially and let the first one have db access
+        # to [potentially] populate it.  if they were to all run at
+        # once, due to the lock and retry, it would still work somewhat,
+        # but it is better to serialize execution explicitly in the task
+        # itself, and avoid reliance on this protection (and eventual
+        # task failure if it were to wait long enough to exceed the lock
+        # acquisition timeout).
+        #
+        makedirs(dirname(CACHELOCK), exist_ok=True)
+        lockmode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH  # 0664
+        lockfd = osopen(CACHELOCK, O_CREAT | O_RDWR, lockmode)
+        tries = 0
+        while True:
+            try: flock(lockfd, LOCK_EX | LOCK_NB)
+            except OSError as e:
+                if e.errno not in [EAGAIN, EWOULDBLOCK, EACCES]:
+                    bomb("unknown errno after failed flock attempt")
+                tries += 1
+                if (tries > LOCKTRIES):
+                    bomb("exclusive cache lock failed after",
+                         LOCKSLEEP * LOCKTRIES, "seconds")
+                sleep(LOCKSLEEP)
+                continue
+            break
+
+        self.tagshelf = shelve.open(CACHEFILE, writeback=True)
+        self.lockfd = lockfd
+        self.runid = ""  # filled by FilterModule.ghreltags()
+
+
+    def __del__(self):
+
+        # remove any keys from prior runs, we only cache one run
+        # todo: cache for some lifetime passed as an arg?
+        #
+        cachekeys = self.tagshelf.keys()
+        for key in cachekeys:
+            if key != self.runid:
+                del self.tagshelf[key]
+
+        self.tagshelf.close()
+        flock(self.lockfd, LOCK_UN)
+
+
 class FilterModule(object):
 
     def filters(self):
         return {'ghreltags': self.ghreltags}
 
-    def ghreltags(self, repodict, ghuser, prefix, apikey=None):
+    def ghreltags(self, repodict, ghuser, prefix, cachekey, apikey=None):
+
+        cache = RelTagsCache()
+        cachedict = cache.tagshelf.get(cachekey) or {}
+        cache.runid = cachekey  # destructor removes other keys
 
         if apikey is not None:
             headers.update({'Authorization': f"Bearer {apikey}"})
@@ -86,25 +197,32 @@ class FilterModule(object):
             if any(c.isdigit() for c in peg):
                 exact = True  # todo: use regex, with user override
 
-            url = f"{GHAPIBASE}/{ghuser}/{project}/tags"
-            url += '?per_page=100'  # default 30, max 100
-            jsdata = []
-            while True:
-                apireq = rq.Request(url)
-                #print(f"apiurl: {url}", file=stderr)
-                #stderr.flush
-                for k, v in headers.items():
-                    apireq.add_header(k, v)
-                with rq.urlopen(apireq) as rs:
-                    links = self.getlinks(rs.headers.get('link', ''))
-                    encoding = rs.headers.get('charset', 'utf-8')
-                    rsdata = rs.read().decode(encoding)
-                jsdata += json.loads(rsdata)
-                nextp = [u.get('url') for u in links if u.get('rel') == 'next']
-                if nextp: url = nextp[0]
-                else: break
+            if (alltags := cachedict.get(project)) is None:
 
-            alltags = [t['name'] for t in jsdata]
+                # not found in cache, so do the [paginated] api call
+                url = f"{GHAPIBASE}/{ghuser}/{project}/tags"
+                url += '?per_page=100'  # default 30, max 100
+                jsdata = []
+                while True:
+                    apireq = rq.Request(url)
+                    #print(f"apiurl: {url}", file=stderr)
+                    #stderr.flush
+                    for k, v in headers.items():
+                        apireq.add_header(k, v)
+                    with rq.urlopen(apireq) as rs:
+                        links = self.getlinks(rs.headers.get('link', ''))
+                        encoding = rs.headers.get('charset', 'utf-8')
+                        rsdata = rs.read().decode(encoding)
+                    jsdata += json.loads(rsdata)
+                    nextp = [u.get('url') for u in links
+                             if u.get('rel') == 'next']
+                    if nextp: url = nextp[0]
+                    else: break
+
+                # extract tags and add to cache so next host can bypass api
+                alltags = [t['name'] for t in jsdata]
+                cachedict[project] = alltags
+
             pfxtags = filter(lambda x: x == peg if exact
                              else x.startswith(peg), alltags)
             try:
@@ -114,7 +232,11 @@ class FilterModule(object):
 
             repodict.update({project: tag})
 
+        # persist anything new we've learned to the shelf
+        cache.tagshelf[cache.runid] = cachedict
+
         return repodict
+
 
     # snarfed from pypi requests library's utils.parse_header_links()
     # license of this function: apache-2.0
@@ -162,6 +284,7 @@ def process_args():
     )
     addarg  (p, 'user', 'github username housing repositories to look up')
     addarg  (p, 'prefix', 'filter out tags not matching this prefix')
+    addarg  (p, 'playid', 'cache key unique to play, reduces api lookups')
     addnarg (p, 'apitok', 'github access token or api key')
 
     args = p.parse_args(args)
@@ -176,7 +299,7 @@ def ghreltags():
 
     repodict = json.load(infile)
     parms = (repodict, args.user, args.prefix)
-    if (t := args.apitok) is not None: parms += (t,)
+    if (tok := args.apitok) is not None: parms += (tok,)
 
     for k, v in func(*parms).items():
         print(f"{k}: {v}")
