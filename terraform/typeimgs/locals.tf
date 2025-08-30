63c2800f92b453b0785696c111576549a4584499
Scott Mcdermott <scott@smemsh.net>
2025-08-30 06:21:46 -0700

    + tf: typeimgs: allfimgs: add u24 image variant
    time to upgrade

--- terraform/typeimgs/locals.tf
+++ terraform/typeimgs/locals.tf
@@ -5,5 +5,10 @@ locals {
   # hardcode rather than derive from plex inventory so the images stay
   # around even if no currently instantiated node exists
   #
-  allfimgs = toset([for v in ["v", "c"] : "u22${v}_adm"])
+  allfimgs = toset(flatten([
+    for vtype in ["v", "c"] : [
+      for ubuver in [22, 24] :
+        format("u%d%s_adm", ubuver, vtype)
+    ]
+  ]))
 }
