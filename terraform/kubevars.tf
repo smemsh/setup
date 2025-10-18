acb917512f7c4a833c95ab46a176a55b0c3f00a1
Scott Mcdermott <scott@smemsh.net>
2025-10-18 08:28:29 -0700

    + kubestrap: install cilium repo/deployment via inline helm values
    instead of flannel (which we prototyped with), cilium is a much more
    complete, capable and performant system.  our install:
    
      - takes over for kube-proxy
      - sets cluster name/id according to podnet/plex (for clustermesh)
      - specifies the podnet in ipam config
      - features:
        - gatewayapi
        - egress gateway
        - dedicated load balancer
        - native routing mode
        - bpf masquerading
        - direct layer 2 announcements
    
    hubble disabled for now, commented out block about it being enabled, we
    have to disable because it requires dataplane nodes to be up, there is
    no tolerance for control-plane taint.  so hubble has to come after
    cluster bootstrap has finished and we have added some dataplane nodes.

--- terraform/kubevars.tf
+++ terraform/kubevars.tf
@@ -3,14 +3,12 @@
 locals {
   kubevers = {
     api = "v1beta4",
-    cni = "v0.27.3",  # flannel
   }
   kubeattrs = {
     token  = "${file("${local.home}/crypt/kube/bootstrap.key")}"
     admrc  = "/etc/kubernetes/kubeadm-config.yml"
     apiver = "apiVersion: kubeadm.k8s.io/${local.kubevers.api}"
     mask   = 16  # pod and service net overlays
-    init_join_timeout = 120
   }
   kubemasters = { for k, v in local.plexhocmap : k => v if v.num == 1 }
   kubeplay = "${local.home}/kubestrap.yml"
